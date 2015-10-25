89bec8bde87af38e444e6be96e343a19b0172be6
Alexander Oryol <eagle.alex@gmail.com>
2015-10-25 23:11:16 +0300

    Show/hide form

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ var app = angular.module('tnTour', []);
 
 app.controller('TourController', function($scope){
 
+  $scope.hiddenForm = true;
+
+  $scope.showForm = function(){
+    console.log('show');
+    $scope.hiddenForm = false;
+  }
+
+  $scope.hideForm = function(){
+    console.log('hide');
+    $scope.hiddenForm = true;
+  }
+
+  $scope.newTour = {title: null, price: null, text: null};
+
+  $scope.addTour = function(newTour){
+
+  }
+
+
 });
