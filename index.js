36765551ea1fc8b37d4d21f65f63d2017c89446f
Alexander Oryol <eagle.alex@gmail.com>
2015-10-26 21:37:26 +0300

    Availability to add tours via form

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ app.controller('TourController', function($scope){
   $scope.hiddenForm = true;
 
   $scope.showForm = function(){
-    console.log('show');
+    $scope.clearForm();
     $scope.hiddenForm = false;
   }
 
   $scope.hideForm = function(){
-    console.log('hide');
     $scope.hiddenForm = true;
   }
 
-  $scope.newTour = {title: null, price: null, text: null};
+  $scope.tours = [
+    {
+      title: 'Карибы на неделю на двоих',
+      price: 54420,
+      text: 'Карибы – это настоящий рай с кокосовыми пальмами, роскошными пляжами с белым и желтым песком, великолепными закатами и уникальным подводным миром. Удивительная природа сочетает в себе массивные вулканы, лазурный океан, кофейные плантации и богатый животный мир.'
+    },
+    {
+      title: 'Гурзуф (Крым) на месяц',
+      price: 100300,
+      text:  'Скалолазание в Крыму — дарит восторг и радость, переполняет эмоциями и пьянит. Вы никогда не забудете свое первое восхождение — это непередаваемое ощущение высоты, адреналин и безграничная свобода собственного тела. Вы непременно захотите испытать это чувство вновь.'
+    }
+  ]
+
+  $scope.clearForm = function(){
+    $scope.newTour = {title: null, price: null, text: null};
+  }
 
   $scope.addTour = function(newTour){
-
+    $scope.tours.push(angular.copy(newTour));
+    $scope.clearForm();
+    $scope.hideForm();
   }
 
 
