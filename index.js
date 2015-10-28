0e600e6e114adbf1113c8c455698aed8820298fd
Alexander Oryol <eagle.alex@gmail.com>
2015-10-28 22:28:48 +0300

    Added localStorage feature

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ app.controller('TourController', function($scope){
     $scope.hiddenForm = true;
   }
 
-  $scope.tours = [
-    {
-      title: 'Карибы на неделю на двоих',
-      price: 54420,
-      text: 'Карибы – это настоящий рай с кокосовыми пальмами, роскошными пляжами с белым и желтым песком, великолепными закатами и уникальным подводным миром. Удивительная природа сочетает в себе массивные вулканы, лазурный океан, кофейные плантации и богатый животный мир.'
-    },
-    {
-      title: 'Гурзуф (Крым) на месяц',
-      price: 100300,
-      text:  'Скалолазание в Крыму — дарит восторг и радость, переполняет эмоциями и пьянит. Вы никогда не забудете свое первое восхождение — это непередаваемое ощущение высоты, адреналин и безграничная свобода собственного тела. Вы непременно захотите испытать это чувство вновь.'
-    }
-  ]
+  if(typeof(localStorage.tours) != 'undefined') {
+    $scope.tours = JSON.parse(localStorage.tours);
+  } else {
+    // initial default tours for example
+    $scope.tours = [
+      {
+        title: 'Карибы на неделю на двоих',
+        price: 54420,
+        text: 'Карибы – это настоящий рай с кокосовыми пальмами, роскошными пляжами с белым и желтым песком, великолепными закатами и уникальным подводным миром. Удивительная природа сочетает в себе массивные вулканы, лазурный океан, кофейные плантации и богатый животный мир.'
+      },
+      {
+        title: 'Гурзуф (Крым) на месяц',
+        price: 100300,
+        text:  'Скалолазание в Крыму — дарит восторг и радость, переполняет эмоциями и пьянит. Вы никогда не забудете свое первое восхождение — это непередаваемое ощущение высоты, адреналин и безграничная свобода собственного тела. Вы непременно захотите испытать это чувство вновь.'
+      }
+    ]
+  }
 
   $scope.emptyTour = function(){
     return {title: null, price: null, text: null};
@@ -40,6 +45,7 @@ app.controller('TourController', function($scope){
     $scope.tours.push(angular.copy(newTour));
     $scope.hideForm();
     $scope.clearForm();
+    localStorage.tours = JSON.stringify($scope.tours);
   }
 
   $scope.deleteTour = function(tour){
@@ -56,6 +62,7 @@ app.controller('TourController', function($scope){
 
   $scope.saveTour = function(tour){
     angular.copy(tour.draft, tour);
+    localStorage.tours = JSON.stringify($scope.tours);
   }
 
   $scope.cancelEdit = function(tour){
