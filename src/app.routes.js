8f3bf1fa782e6ad2649ae52922587e0e42c2b592
Alexander Oryol <eagle.alex@gmail.com>
2015-12-05 12:11:29 +0300

    Reorder project files for specs

--- src/app.routes.js
+++ src/app.routes.js
@@ -2,30 +2,30 @@ angular.module('tn-routing', ['ngRoute', 'ngResource'])
   .config(function($routeProvider, $locationProvider, $httpProvider){
     $routeProvider
     .when('/', {
-      templateUrl: "app/components/home/index.html",
+      templateUrl: "components/home/index.html",
       controller: 'HomeController',
     })
     .when('/tours/:objectId', {
-      templateUrl: 'app/components/home/tour.html',
+      templateUrl: 'components/home/tour.html',
       controller: 'TourController'
     })
     .when('/adm/tours', {
-      templateUrl: "app/components/tour/index.html",
+      templateUrl: "components/tour/index.html",
       controller: 'ToursController',
       admFlag: true,
     })
     .when('/adm/countries', {
-      templateUrl: 'app/components/country/countries.html',
+      templateUrl: 'components/country/countries.html',
       controller: 'CountriesController',
       admFlag: true
     })
     .when('/adm/places', {
-      templateUrl: 'app/components/place/places.html',
+      templateUrl: 'components/place/places.html',
       controller: 'PlacesController',
       admFlag: true
     })
     .when('/adm/hotels', {
-      templateUrl: 'app/components/hotel/hotels.html',
+      templateUrl: 'components/hotel/hotels.html',
       controller: 'HotelsController',
       admFlag: true
     })
