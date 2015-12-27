e8b6184b6412dd246217864160796b04cbd9d140
Alexander Oryol <eagle.alex@gmail.com>
2015-12-27 22:47:05 +0300

    Order test files

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       'node_modules/angular-resource/angular-resource.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'src/**/*.js',
-      'test/*.js',
+      'test/**/*.js',
       'src/**/*.html'
     ],
 
