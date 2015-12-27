b98c3a972db4cbbf890ccb8c8df2f347907c1fe8
Alexander Oryol <eagle.alex@gmail.com>
2015-12-27 23:22:22 +0300

    First Country resource specs

---
+++ test/resources/country_resource.spec.js
@@ -0,0 +1,29 @@
+describe('TourController', function(){
+  beforeEach(module('tnTour'));
+
+  var Country;
+  var APIurl = "https://api.parse.com/1/classes/Country";
+  var $httpBackend;
+
+  beforeEach(inject(function(_Country_, _$httpBackend_){
+    Country = _Country_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  describe('initialize', function(){
+    it('expect call to parse.com', function(){
+      $httpBackend.expectGET(APIurl).respond(200);
+      expect($httpBackend.verifyNoOutstandingExpectation).not.toThrow();
+    });
+    it('parse and sort results', function(){
+      var andorra = { objectId: 'a1', name: 'Andorra' };
+      var cuba = { objectId: 'b1', name: 'Cuba' };
+      var response = { results: [cuba, andorra] };
+      $httpBackend.expectGET(APIurl).respond(200, response);
+      $httpBackend.flush();
+      expect(Country.all().length).toBe(2);
+      expect(Country.all()[0].name).toBe(andorra.name);
+    });
+  });
+
+});
